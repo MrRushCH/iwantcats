a20eccfd6954112a6ef320b9352117f60017a705
Marc Rusch <mrusch@c18306.namics.home>
2019-10-15 14:26:43 +0200

    Remove console.logs

--- images.js
+++ images.js
@@ -8,7 +8,6 @@ export function replaceImages() {
     if (item.nodeName === "IMG") {
       let imgWidth = item.width;
       let imgHeight = item.height;
-      console.log("width:", imgWidth, "height:", imgHeight);
       if (imgHeight !== 0) {
         item.src = `https://picsum.photos/${imgWidth}/${imgHeight}?time=${date.getTime()}`;
       } else {
@@ -17,7 +16,6 @@ export function replaceImages() {
     } else if ($item.css("background-image").indexOf("url") != -1) {
       item.style.backgroundImage = `url('https://picsum.photos/2000?time=${date.getTime()}')`;
       $item.css("background-size", "cover");
-      console.log("yes");
     }
   });
 }
